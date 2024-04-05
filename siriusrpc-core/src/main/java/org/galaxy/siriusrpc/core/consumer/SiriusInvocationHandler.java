43e8145f7c72c019427916e19e6747df606611f6
Albertsirius <hzhsirius@gmail.com>
2024-04-05 21:12:03 +0800

    6th: refactor http invoker

--- siriusrpc-core/src/main/java/org/galaxy/siriusrpc/core/consumer/SiriusInvocationHandler.java
+++ siriusrpc-core/src/main/java/org/galaxy/siriusrpc/core/consumer/SiriusInvocationHandler.java
@@ -1,22 +1,14 @@
 package org.galaxy.siriusrpc.core.consumer;
 
-import com.alibaba.fastjson.JSON;
-import okhttp3.ConnectionPool;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
 import org.galaxy.siriusrpc.core.api.RpcContext;
 import org.galaxy.siriusrpc.core.api.RpcRequest;
 import org.galaxy.siriusrpc.core.api.RpcResponse;
 import org.galaxy.siriusrpc.core.util.MethodUtils;
 import org.galaxy.siriusrpc.core.util.TypeUtils;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -28,7 +20,8 @@ public class SiriusInvocationHandler implements InvocationHandler {
     RpcContext context;
     List<String> providers;
 
-    final static MediaType JSONTYPE =  MediaType.get("application/json; charset=utf-8");
+    HttpInvoker httpInvoker = new OkhttpInvoker();
+
 
     public SiriusInvocationHandler(Class<?> clazz, RpcContext context, List<String> providers) {
         this.service = clazz;
@@ -50,37 +43,13 @@ public class SiriusInvocationHandler implements InvocationHandler {
 
         List<String> urls = context.getRouter().rout(providers);
         String url = (String) context.getLoadBalancer().choose(urls);
-        RpcResponse rpcResponse = post(rpcRequest, url);
+        RpcResponse<?> rpcResponse = httpInvoker.post(rpcRequest, url);
         if (rpcResponse.isStatus()) {
             Object data = rpcResponse.getData();
             return TypeUtils.castMethodResult(method, data);
-        }else {
+        } else {
             Exception exception = rpcResponse.getEx();
-            //exception.printStackTrace();
             throw exception;
-        }
-    }
-
-    OkHttpClient client = new OkHttpClient.Builder()
-            .connectionPool(new ConnectionPool(16, 60, TimeUnit.SECONDS))
-            .readTimeout(1, TimeUnit.SECONDS)
-            .writeTimeout(1, TimeUnit.SECONDS)
-            .connectTimeout(1, TimeUnit.SECONDS)
-            .build();
-
-    private RpcResponse post(RpcRequest rpcRequest, String url) {
-        String reqJson = JSON.toJSONString(rpcRequest);
-        Request request = new Request.Builder()
-                .url(url)
-                .post(RequestBody.create(reqJson, JSONTYPE))
-                .build();
-        try {
-            String resp = client.newCall(request).execute().body().string();
-            RpcResponse rpcResponse = JSON.parseObject(resp, RpcResponse.class);
-            return rpcResponse;
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
         }
     }
 }
